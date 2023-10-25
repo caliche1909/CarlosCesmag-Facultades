c1a33be2dd87921cf77165f3d426c125ac9f7586
Carlos Moran <karyngadm@gmai.com>
2023-10-25 09:15:57 -0500

    se agrego la tabla

---
+++ facultades.php
@@ -0,0 +1,53 @@
+<?php
+    include 'index.php'
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>FACULTADES</h1>
+
+    </div>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Num.</th>
+      <th scope="col">Codigo</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Opciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>12345</td>
+      <td>Carlos Andres</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>54321</td>
+      <td>Carolina Lopez</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+        <th scope="row">2</th>
+        <td>54321</td>
+        <td>Carolina Lopez</td>
+        <td>@fat</td>
+      </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+</body>
+</html>
