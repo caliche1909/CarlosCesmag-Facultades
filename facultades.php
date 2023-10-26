271a6fe8225a489dab502e4d24f4aaae57502d03
Carlos Moran <karyngadm@gmai.com>
2023-10-25 21:30:48 -0500

    miercoles 25 14:12

--- facultades.php
+++ facultades.php
@@ -8,73 +8,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 
 <body>
-    <div>
+    <div class=" container">
         <h1>FACULTADES</h1>
 
     </div>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Num.</th>
-                <th scope="col">Codigo</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Opciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-        $sql = "SELECT * FROM facultades";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>".$row["codigo_fac"]."</td>";
-                echo "<td>".$row["nombre_fac"]."</td>";
-                echo "<td>
-                    <a class='btn btn-success'>Editar</a>
-                    <a class='btn btn-danger'>Eliminar</a>
-                    </td>";
-                echo "</tr>";
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    
+                    <th scope="col">Codigo</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Opciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+            $sql = "SELECT * FROM facultad";
+            $result = $conn->query($sql);
+    
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>".$row["Codigo_Fac"]."</td>";
+                    echo "<td>".$row["Nombre_Fac"]."</td>";
+                    echo "<td>
+                        <a class='btn btn-success'>Editar</a>
+                        <a class='btn btn-danger'>Eliminar</a>
+                        </td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "0 resultados";
             }
-        } else {
-            echo "0 resultados";
-        }
-        $conn->close();
-    ?>
-            <tr>
-                <th scope="row">1</th>
-                <td>12345</td>
-                <td>Carlos Andres</td>
-                <td>
-                    <a class="btn btn-outline-success btn-rounded"  data-mdb-ripple-color="dark">editar</a>
-                    <button type="button" class="btn btn-outline-danger btn-rounded"
-                        data-mdb-ripple-color="dark">Danger</button>
-                </td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>54321</td>
-                <td>Carolina Lopez</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>54321</td>
-                <td>Carolina Lopez</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-            </tr>
-        </tbody>
-    </table>
+            $conn->close();
+        ?>
+               
+            </tbody>
+        </table>
+    </div>
 
 </body>
 
